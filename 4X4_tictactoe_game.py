b02d7e316531c9730d9c6e788260d82a5a45f1a1
JinHyunjeong <108727284+JobHJ@users.noreply.github.com>
2022-12-07 15:48:34 +0900

    Update 4X4_tictactoe_game.py
    Add english annotation

--- 4X4_tictactoe_game.py
+++ 4X4_tictactoe_game.py
@@ -1,21 +1,21 @@
 import pygame  # 1. pygame 선언
 import os
 
-# 현재 위치 정의
+# 현재 위치 정의 (Define Current Location)
 current_path = os.path.dirname(__file__)
-# 이미지 폴더 위치 정의
+# 이미지 폴더 위치 정의 (Define Image Folder Location)
 image_path = os.path.join(current_path, "images")
-# 불러올 이미지 로드
+# 불러올 이미지 로드 (Loading images to be recalled)
 background = pygame.image.load(os.path.join(image_path, "background.jpg"))
 
-pygame.init()  # 2. pygame 초기화
-# 3. pygame에 사용되는 전역변수 선언
+pygame.init()  # 2. pygame 초기화 (pygame initialization)
+# 3. pygame에 사용되는 전역변수 선언 (Declare global variables used in pygame)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
-PALEYELLOW = (255, 255, 153)  # white
-Light_turquoise = (224, 255, 255)  # YELLOW
+PALEYELLOW = (255, 255, 153) 
+Light_turquoise = (224, 255, 255)  
 
 large_font = pygame.font.SysFont(None, 100)
 small_font = pygame.font.SysFont(None, 100)
@@ -25,7 +25,7 @@ turn = 0
 grid = [' ', ' ', ' ', ' ',
         ' ', ' ', ' ', ' ',
         ' ', ' ', ' ', ' ',
-        ' ', ' ', ' ', ' ']
+        ' ', ' ', ' ', ' '] #게임 판(game board)
 done = False
 clock = pygame.time.Clock()
 
@@ -66,7 +66,7 @@ turn = 0
 
 
 def runGame():
-    # 게임 활용 변수
+    # 게임 활용 변수 (Game Utilization Variables)
     CELL_SIZE = 100
     COLUMN_COUNT = 4
     ROW_COUNT = 4
@@ -119,7 +119,7 @@ def runGame():
                             break
                         turn += 1
                         turn = turn % 2
-            # 화면 그리기
+            # 화면 그리기 (Drawing a screen)
             for column_index in range(COLUMN_COUNT):
                 for row_index in range(ROW_COUNT):
                     rect = (CELL_SIZE * column_index, CELL_SIZE * row_index, CELL_SIZE, CELL_SIZE)
@@ -155,7 +155,7 @@ def runGame():
                     game_over_image = large_font.render('Draw', True, RED)
                 screen.blit(game_over_image,
                             (600 // 2 - game_over_image.get_width() // 2, 600 // 2 - game_over_image.get_height() // 2))
-            pygame.display.update()  # 모든 화면 그리기 업데이트
+            pygame.display.update()  # 모든 화면 그리기 업데이트 (Update all screen drawings)
             if count==1:
                 pygame.quit()
 
